2b981d18eb5559831bcfb34b62e62e81bb61ed72
(J)AVA <91890643+Avalhea@users.noreply.github.com>
2021-12-07 12:11:06 +0100

    mini modif connexion

--- src/eni/fr/javaee/projet/servlet/Connexion.java
+++ src/eni/fr/javaee/projet/servlet/Connexion.java
@@ -1,6 +1,8 @@
 package eni.fr.javaee.projet.servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class ui
  */
-@WebServlet("/ui")
+@WebServlet("/connexion")
 public class Connexion extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -26,16 +28,9 @@ public class Connexion extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/WEB-INF/connexion.jsp");
+		dispatcher.forward(request, response);
 	}
 
 }
