6d9e079ab8a5c7b73c401c5b8d447be45f08a9d0
Marc-Annezo <91891149+Marc-Annezo@users.noreply.github.com>
2021-12-07 12:34:41 +0100

    Modif nom

--- src/eni/fr/javaee/projet/servlet/Connexion.java
+++ src/eni/fr/javaee/projet/servlet/Connexion.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class ui
  */
-@WebServlet("/connexion")
+@WebServlet("/connexionServlet")
 public class Connexion extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
