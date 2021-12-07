693bf76af6130ab71de3f066992be1a15da02a3d
Thomas Arnaud <thomas.arnaud2021@campus-eni.fr>
2021-12-07 14:44:00 +0100

    Update UtilisateurManager.java

--- src/eni/fr/javaee/projet/bll/UtilisateurManager.java
+++ src/eni/fr/javaee/projet/bll/UtilisateurManager.java
@@ -6,39 +6,39 @@ import fr.eni.javaee.projet.dal.DALException;
 import fr.eni.javaee.projet.dal.ProjetDAOFactory;
 
 public class UtilisateurManager {
-	
-	//Création du singleton
-	
-private static UtilisateurManager instance;
-	
+
+	// Création du singleton
+
+	private static UtilisateurManager instance;
+
 	private static UtilisateurDAO dao;
-	
+
 	private UtilisateurManager() {
 		dao = ProjetDAOFactory.getUtilisateurDAO();
 	};
 
 	public static UtilisateurManager getInstance() {
-		if(instance == null) {
+		if (instance == null) {
 			instance = new UtilisateurManager();
 		}
 		return instance;
 	}
-	
+
 	public Utilisateur afficherProfil(String pseudo) throws DALException {
-		
+
 		Utilisateur utilisateur = dao.afficherProfil(pseudo);
 		return utilisateur;
-		
+
 	}
-	
+
 	public Utilisateur insertUtilisateur(Utilisateur nouvelUtilisateur) throws DALException {
 		Utilisateur utilisateur = dao.insertUtilisateur(nouvelUtilisateur);
 		return utilisateur;
-		
+
 	}
-	
+
 	public String afficherMotDePasse(String pseudo) throws DALException {
-		
+
 		String motDePasse = dao.afficherMotDePasse(pseudo);
 		return motDePasse;
 	}
