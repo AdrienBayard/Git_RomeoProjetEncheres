8739eb5563df5729e9ddf3572c22199ef2d1e603
Thomas Arnaud <thomas.arnaud2021@campus-eni.fr>
2021-12-07 16:36:06 +0100

    Push manager

--- src/eni/fr/javaee/projet/bll/UtilisateurManager.java
+++ src/eni/fr/javaee/projet/bll/UtilisateurManager.java
@@ -24,8 +24,8 @@ public class UtilisateurManager {
 		return instance;
 	}
 
-	public Utilisateur afficherProfil(String pseudo) throws BLLException{
-		
+	public Utilisateur afficherProfil(String pseudo) throws BLLException {
+
 		BLLException ex = new BLLException();
 		validationPseudo(pseudo, ex);
 		Utilisateur utilisateur = null;
@@ -45,7 +45,15 @@ public class UtilisateurManager {
 		BLLException ex = new BLLException();
 		validationPseudo(pseudo, ex);
 		validationNom(nom, ex);
-		
+		validationPrenom(prenom, ex);
+		validationEmail(email, ex);
+		validationTelephone(telephone, ex);
+		validationRue(rue, ex);
+		validationCodePostal(codePostal, ex);
+		validationVille(ville, ex);
+		validationMotDePasse(motDePasse, ex);
+		utilisateur = new Utilisateur(pseudo, nom, prenom, email, telephone, rue, codePostal, ville, motDePasse);
+
 		try {
 			utilisateur = dao.insertUtilisateur(utilisateur);
 		} catch (DALException e) {
@@ -57,7 +65,8 @@ public class UtilisateurManager {
 	}
 
 	public String afficherMotDePasse(String pseudo) throws BLLException {
-
+		BLLException ex = new BLLException();
+		validationPseudo(pseudo, ex);
 		String motDePasse = null;
 		try {
 			motDePasse = dao.afficherMotDePasse(pseudo);
@@ -89,12 +98,20 @@ public class UtilisateurManager {
 	}
 
 	public void updateUtilisateur(String pseudo, String nom, String prenom, String email, String telephone, String rue,
-			String codePostal, String ville) throws BLLException {
+			String codePostal, String ville, String motDePasse) throws BLLException {
 
 		BLLException ex = new BLLException();
-
+		validationPseudo(pseudo, ex);
+		validationNom(nom, ex);
+		validationPrenom(prenom, ex);
+		validationEmail(email, ex);
+		validationTelephone(telephone, ex);
+		validationRue(rue, ex);
+		validationCodePostal(codePostal, ex);
+		validationVille(ville, ex);
+		validationMotDePasse(motDePasse, ex);
 		Utilisateur nouvelUtilisateur = null;
-		nouvelUtilisateur = new Utilisateur(pseudo, nom, prenom, email, telephone, rue, codePostal, ville);
+		nouvelUtilisateur = new Utilisateur(pseudo, nom, prenom, email, telephone, rue, codePostal, ville, motDePasse);
 
 		if (ex.hasErreur()) {
 			throw ex;
@@ -115,56 +132,68 @@ public class UtilisateurManager {
 			ex.ajouterErreur(new ParameterException("L'id doit Etre un entier positif >= 1"));
 		}
 	}
-	private void validationPseudo(String pseudo, BLLException ex)throws BLLException {
-		if (pseudo == null || pseudo.isEmpty()|| pseudo.length() > 30) {
-			ex.ajouterErreur(new ParameterException("Le pseudo est obligatoire et doit avoir une longueur comprise entre 1 et 30"));
+
+	private void validationPseudo(String pseudo, BLLException ex) throws BLLException {
+		if (pseudo == null || pseudo.isEmpty() || pseudo.length() > 30) {
+			ex.ajouterErreur(new ParameterException(
+					"Le pseudo est obligatoire et doit avoir une longueur comprise entre 1 et 30"));
 		}
 	}
-	private void validationNom(String nom, BLLException ex)throws BLLException {
-		if (nom == null || nom.isEmpty()|| nom.length() > 30) {
-			ex.ajouterErreur(new ParameterException("Le nom est obligatoire et doit avoir une longueur comprise entre 1 et 30"));
+
+	private void validationNom(String nom, BLLException ex) throws BLLException {
+		if (nom == null || nom.isEmpty() || nom.length() > 30) {
+			ex.ajouterErreur(
+					new ParameterException("Le nom est obligatoire et doit avoir une longueur comprise entre 1 et 30"));
 		}
 	}
-	private void validationPrenom(String prenom, BLLException ex)throws BLLException {
-		if (prenom == null || prenom.isEmpty()|| prenom.length() > 30) {
-			ex.ajouterErreur(new ParameterException("Le prenom est obligatoire et doit avoir une longueur comprise entre 1 et 30"));
+
+	private void validationPrenom(String prenom, BLLException ex) throws BLLException {
+		if (prenom == null || prenom.isEmpty() || prenom.length() > 30) {
+			ex.ajouterErreur(new ParameterException(
+					"Le prenom est obligatoire et doit avoir une longueur comprise entre 1 et 30"));
 		}
 	}
-	private void validationEmail(String email, BLLException ex)throws BLLException {
-		if (email == null || email.isEmpty()|| email.length() > 30) {
-			ex.ajouterErreur(new ParameterException("L'email est obligatoire et doit avoir une longueur comprise entre 1 et 30"));
+
+	private void validationEmail(String email, BLLException ex) throws BLLException {
+		if (email == null || email.isEmpty() || email.length() > 30) {
+			ex.ajouterErreur(new ParameterException(
+					"L'email est obligatoire et doit avoir une longueur comprise entre 1 et 30"));
 		}
 	}
-	private void validationTelephone(String telephone, BLLException ex)throws BLLException {
-		if (telephone == null || telephone.isEmpty()|| telephone.length() > 15) {
-			ex.ajouterErreur(new ParameterException("Le numero de telephone est obligatoire et doit avoir une longueur comprise entre 1 et 15"));
+
+	private void validationTelephone(String telephone, BLLException ex) throws BLLException {
+		if (telephone == null || telephone.isEmpty() || telephone.length() > 15) {
+			ex.ajouterErreur(new ParameterException(
+					"Le numero de telephone est obligatoire et doit avoir une longueur comprise entre 1 et 15"));
 		}
 	}
-	private void validationRue(String rue, BLLException ex)throws BLLException {
-		if (rue == null || rue.isEmpty()|| rue.length() > 15) {
-			ex.ajouterErreur(new ParameterException("La rue est obligatoire et doit avoir une longueur comprise entre 1 et 30"));
+
+	private void validationRue(String rue, BLLException ex) throws BLLException {
+		if (rue == null || rue.isEmpty() || rue.length() > 15) {
+			ex.ajouterErreur(
+					new ParameterException("La rue est obligatoire et doit avoir une longueur comprise entre 1 et 30"));
 		}
 	}
-	private void validationCodePostal(String codePostal, BLLException ex)throws BLLException {
-		if (codePostal == null || codePostal.isEmpty()|| codePostal.length() > 10) {
-			ex.ajouterErreur(new ParameterException("Le code postal est obligatoire et doit avoir une longueur comprise entre 1 et 10"));
+
+	private void validationCodePostal(String codePostal, BLLException ex) throws BLLException {
+		if (codePostal == null || codePostal.isEmpty() || codePostal.length() > 10) {
+			ex.ajouterErreur(new ParameterException(
+					"Le code postal est obligatoire et doit avoir une longueur comprise entre 1 et 10"));
 		}
 	}
-	private void validationVille(String ville, BLLException ex)throws BLLException {
-		if (ville == null || ville.isEmpty()|| ville.length() > 50) {
-			ex.ajouterErreur(new ParameterException("La ville est obligatoire et doit avoir une longueur comprise entre 1 et 50"));
+
+	private void validationVille(String ville, BLLException ex) throws BLLException {
+		if (ville == null || ville.isEmpty() || ville.length() > 50) {
+			ex.ajouterErreur(new ParameterException(
+					"La ville est obligatoire et doit avoir une longueur comprise entre 1 et 50"));
 		}
 	}
-	
-	private void validationMotDePasse(String motDePasse, BLLException ex)throws BLLException {
-		if (motDePasse == null || motDePasse.isEmpty()|| motDePasse.length() > 30) {
-			ex.ajouterErreur(new ParameterException("La ville est obligatoire et doit avoir une longueur comprise entre 1 et 30"));
+
+	private void validationMotDePasse(String motDePasse, BLLException ex) throws BLLException {
+		if (motDePasse == null || motDePasse.isEmpty() || motDePasse.length() > 30) {
+			ex.ajouterErreur(new ParameterException(
+					"La ville est obligatoire et doit avoir une longueur comprise entre 1 et 30"));
 		}
 	}
-	
-	
-	
-	
-	
-	
+
 }
