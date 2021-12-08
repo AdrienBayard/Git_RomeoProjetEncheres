cdd282b9e36d169f9ec80ba92da974b35c2cc484
Marc-Annezo <91891149+Marc-Annezo@users.noreply.github.com>
2021-12-08 12:01:30 +0100

    connexionServlet INSERT

--- src/eni/fr/javaee/projet/servlet/ConnexionServlet.java
+++ src/eni/fr/javaee/projet/servlet/ConnexionServlet.java
@@ -15,51 +15,49 @@ import eni.fr.javaee.projet.bll.UtilisateurManager;
 import eni.fr.javaee.projet.bo.Utilisateur;
 import fr.eni.javaee.projet.dal.DALException;
 
-
 /**
  * Servlet implementation class ui
  */
 @WebServlet("/connexionServlet")
 public class ConnexionServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ConnexionServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public ConnexionServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
-		
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		String pseudo = request.getParameter("pseudo");
 		String motDePasse = request.getParameter("motDePasse");
 		String laJsp = null;
 		Boolean mdpValide = true;
 		String leMdp = null;
-		
-		//		faire appel à la méthode sql pour récupérer le motDePasse d'un pseudo 
+
+		// faire appel à la méthode sql pour récupérer le motDePasse d'un pseudo
 		try {
 			leMdp = UtilisateurManager.getInstance().afficherMotDePasse(pseudo);
 		} catch (BLLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
-		if(motDePasse.equals(leMdp)) {
+
+		if (motDePasse.equals(leMdp)) {
 			mdpValide = true;
 			laJsp = "/WEB-INF/jsp/connected.jsp";
-		}
-		else {
+		} else {
 			mdpValide = false;
 			laJsp = "/WEB-INF/jsp/connexion.jsp";
-			
+
 		}
 		request.setAttribute("mdpValide", mdpValide);
 		RequestDispatcher aiguilleur = getServletContext().getRequestDispatcher(laJsp);
@@ -67,35 +65,55 @@ public class ConnexionServlet extends HttpServlet {
 	}
 
 	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
-		// Récupération des informations du client depuis le formulaire.  
-		String pseudo = req.getParameter("pseudo"); 
-		String nom = req.getParameter("nom");
-		String prenom = req.getParameter("prenom");
-		String telephone = req.getParameter("telephone");
-		String rue = req.getParameter("rue");
-		String codePostal = req.getParameter("codePostal");
-		String ville = req.getParameter("ville");
-		String motDePasse = req.getParameter("motDePasse");
-		String confirmation = req.getParameter("confirmation");
-		
-		// Récupération des pseudos et mail déjà existants. 
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		Boolean inscription = true;
+		int msgErreur = 0;
+
+		// Récupération des informations du client depuis le formulaire.
+		String pseudo = request.getParameter("pseudo");
+		String nom = request.getParameter("nom");
+		String prenom = request.getParameter("prenom");
+		String email = request.getParameter("email");
+		String telephone = request.getParameter("telephone");
+		String rue = request.getParameter("rue");
+		String codePostal = request.getParameter("codePostal");
+		String ville = request.getParameter("ville");
+		String motDePasse = request.getParameter("motDePasse");
+		String confirmation = request.getParameter("confirmation");
+
+		// Blocage en cas de mdp - confirmation sont différents.
+		if (motDePasse != confirmation) {
+			msgErreur = 1; // Msg : le mdp et la confirmation doivent être identiques. 
+		}
+
+		// Récupération des pseudos et mail déjà existants.
+		List<Utilisateur> listUtilExistants;
 		try {
-			List<Utilisateur> listUtilExistants = UtilisateurManager.getInstance().getListeUtilisateurs();
-			
-			for(Utilisateur user : listUtilExistants) {
-				System.out.println(user.toString());
+			listUtilExistants = UtilisateurManager.getInstance().getListeUtilisateurs();
+			for (Utilisateur user : listUtilExistants) {
+				if ((user.getPseudo() != pseudo) && (user.getEmail() != email)) {
+					inscription = true;
+				} else {
+					inscription = false;
+				}
 			}
+
+			if (inscription == true) {
+				UtilisateurManager.getInstance().insertUtilisateur(pseudo, nom, prenom, email,telephone, rue, codePostal, ville, motDePasse);
+				RequestDispatcher aiguilleur = getServletContext().getRequestDispatcher("/WEB-INF/jsp/connected.jsp");
+				aiguilleur.forward(request, response);
+			} else if (inscription == false) {
+				msgErreur = 2; // Msg : Pseudo ou adresse mail déjà utilisés.
+				RequestDispatcher aiguilleur = getServletContext().getRequestDispatcher("/WEB-INF/jsp/inscription.jsp");
+				aiguilleur.forward(request, response);
+			}
+
 		} catch (BLLException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} 
-		
-		if(!motDePasse.equals(confirmation))
-		
-		super.doPost(req, resp);
+		}
+
 	}
-	
-	
 
 }
