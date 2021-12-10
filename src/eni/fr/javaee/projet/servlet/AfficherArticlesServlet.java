05b1d54b60def11b20894d5271b965c817cb78ab
Thomas Arnaud <thomas.arnaud2021@campus-eni.fr>
2021-12-10 12:55:20 +0100

    zae

--- src/eni/fr/javaee/projet/servlet/AfficherArticlesServlet.java
+++ src/eni/fr/javaee/projet/servlet/AfficherArticlesServlet.java
@@ -1,6 +1,8 @@
 package eni.fr.javaee.projet.servlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,26 +11,36 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import eni.fr.javaee.projet.bll.ArticleManager;
+import eni.fr.javaee.projet.bo.ArticleVendu;
+import fr.eni.javaee.projet.dal.DALException;
+
 /**
  * Servlet implementation class AfficherArticlesServlet
  */
-@WebServlet(name = "AfficherArticles", urlPatterns = { "/accueilServlet" })
+@WebServlet(name = "AfficherArticles", urlPatterns = { "/accueil" })
 public class AfficherArticlesServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public AfficherArticlesServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+  
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("hello");
+//		System.out.println("hello");
+//		List<ArticleVendu> listeArticles = new ArrayList<ArticleVendu>();
+//		try {
+//		 listeArticles  = (List<ArticleVendu>) ArticleManager.getInstance().afficherAchatsEnCours();
+//		} catch (DALException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		for (ArticleVendu articleVendu : listeArticles) {
+//			System.out.println("test : " + articleVendu.toString());
+//		}
+//		System.out.println("test 2 : " +listeArticles.toString());
+//		request.setAttribute("listeArticles", listeArticles);
 		RequestDispatcher aiguilleur = getServletContext().getRequestDispatcher("/pageAccueil");
 		aiguilleur.forward(request, response);
 	}
