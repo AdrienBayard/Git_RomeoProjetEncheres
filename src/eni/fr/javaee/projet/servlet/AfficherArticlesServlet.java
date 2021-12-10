b85ccfeeb939aaae08c391914ea3c5d18b19d89a
Thomas Arnaud <thomas.arnaud2021@campus-eni.fr>
2021-12-10 11:56:18 +0100

    Create AfficherArticlesServlet.java

---
+++ src/eni/fr/javaee/projet/servlet/AfficherArticlesServlet.java
@@ -0,0 +1,44 @@
+package eni.fr.javaee.projet.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AfficherArticlesServlet
+ */
+@WebServlet(name = "AfficherArticles", urlPatterns = { "/accueilServlet" })
+public class AfficherArticlesServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AfficherArticlesServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("hello");
+		RequestDispatcher aiguilleur = getServletContext().getRequestDispatcher("/pageAccueil");
+		aiguilleur.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
