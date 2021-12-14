19ffa372696b67f33d4ff64fb5f0a78926d59493
Marc-Annezo <91891149+Marc-Annezo@users.noreply.github.com>
2021-12-14 11:30:03 +0100

    Update AfficherConnectedArticleServlet.java

--- src/eni/fr/javaee/projet/servlet/AfficherConnectedArticleServlet.java
+++ src/eni/fr/javaee/projet/servlet/AfficherConnectedArticleServlet.java
@@ -17,7 +17,7 @@ import eni.fr.javaee.projet.bo.ArticleVendu;
 /**
  * Servlet implementation class AfficherConnectedArticleServlet
  */
-@WebServlet(name = "afficherconnected", urlPatterns = { "/afficherConnected" })
+@WebServlet(name = "afficherconnected", urlPatterns = { "/afficherconnected" })
 public class AfficherConnectedArticleServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -53,40 +53,23 @@ public class AfficherConnectedArticleServlet extends HttpServlet {
 		String huhuh = request.getParameter("encheresOuvertes"); 
 		System.out.println("value enchère ouvertes : " + huhuh); 
 		
-//		Boolean zboui = Boolean.valueOf(request.getParameter("encheresOuvertes")); 
-//		
-//		Boolean test2 = zboui.booleanValue(); 
-//		
-//		if(test2) {
-//			System.out.println("true");
-//		} else {
-//			System.out.println("false");
-//		}
-		Boolean test3; 		
-		
-		if(request.getParameter("encheresOuvertes") == null) {
-			test3 = false; 
-			
-			System.out.println("Youpi");
-		} else {
-			test3 = true; 
-			System.out.println("Pas de chance");
-		}
-		
 		
 		
 		// Afficher les articles d'une certaine catégorie
 		
 		// Dans "mes achats"
-		if(request.getParameter("achats") != null) {
+		if((request.getParameter("ventesAchats") != null) && (request.getParameter("ventesAchats").equals("achats"))) {
 			System.out.println("entrée achat");
-			if(request.getParameter("encheresOuvertes")!= null) {
+			
+			if((request.getParameter("mesEncheres") != null) && (request.getParameter("encheresOuvertes").equals("ON"))) {
 				//Afficher Uniquement les enchères ouvertes (l'utilisateur doit être vendeur + la date d'échéance doit être ouverte)
 				System.out.println("achat/enchereouvertes");
-			} else if((request.getParameter("mesEncheres") == "true") && (request.getParameter("mesEncheres")!= null)) {
+			}
+			if((request.getParameter("mesEncheres") != null) && (request.getParameter("mesEncheres").equals("ON"))) {
 				// Afficher uniquement mes enchères (Uniquement les enchêres où l'utilisateur est intervenu + la date d'échéance doit être ouverte)
 				System.out.println("achat/mes encheres");
-			} else if((request.getParameter("mesEncheresRemportees") != null) && (request.getParameter("mesEncheresRemportees").equals("true"))){
+			}
+			if((request.getParameter("mesEncheresRemportees") != null) && (request.getParameter("mesEncheresRemportees").equals("ON"))){
 				// Afficher uniquement mes enchères remportées (Uniquement les enchêres où l'utilisateur est le meilleur encherisseur et la date doit être fermée).
 				System.out.println("achat/mer");
 			}	
@@ -95,15 +78,19 @@ public class AfficherConnectedArticleServlet extends HttpServlet {
 		
 		
 		//Dans "mes ventes"
-		if(request.getParameter("ventes") != null) {
-			if(request.getParameter("mesVentesEnCours") != null) {
+		if((request.getParameter("ventesAchats") != null) && (request.getParameter("ventesAchats").equals("ventes"))) {
+			System.out.println("youpi");
+			if((request.getParameter("mesVentesEnCours") != null) && (request.getParameter("mesVentesEnCours").equals("ON"))) {
+				System.out.println("youpi");
 				// Afficher uniquement mes ventes en cours (Uniquement si l'utilisateur est vendeur + la dateFinEnchere n'est pas arrivée à terme). 
-			} else if(request.getParameter("ventesNonDebutees") != null) {
+			}
+			if((request.getParameter("ventesNonDebutees") != null) && (request.getParameter("ventesNonDebutees").equals("ON"))) {
 				// Afficher uniquement mes ventes non débutées (Uniquement si l'utilisateur est vendeur + la dateDebutEnchere n'est pas arrivée)
-
-			} else if(request.getParameter("ventesTerminees") != null) {
+				System.out.println("youpi");
+			}
+			if((request.getParameter("ventesTerminees") != null) && (request.getParameter("ventesTerminees").equals("ON"))) {
 				// Afficher uniquement mes ventes non débutées (Uniquement si l'utilisateur est vendeur + la dateFinEnchere est dépassée)
-
+				System.out.println("youpi");
 			}	
 		}
 
@@ -119,3 +106,4 @@ public class AfficherConnectedArticleServlet extends HttpServlet {
 	}
 
 }
+
